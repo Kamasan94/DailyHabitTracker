92f070bfa3361ecfdef738b6c6e7e4ca092aa563
Kamasan94 <pooldaimon94@gmail.com>
2025-06-26 12:31:38 +0200

    Habits and relations

--- server/src/models/Habit.ts
+++ server/src/models/Habit.ts
@@ -1,36 +1,24 @@
-import { Schema, model, connect } from 'mongoose';
+import { Schema, model} from 'mongoose';
 
 // 1. Create an interface representing a document in MongoDB.
 interface IHabit {
   name: string;
   color: string;
   activeDays: number[];
+  user: Schema.Types.ObjectId;
 }
 
 // 2. Create a Schema corresponding to the document interface.
 const habitSchema = new Schema<IHabit>({
   name: { type: String, required: true },
   color: { type: String, required: true },
-  activeDays: { type: [Number], required: true}
-});
+  activeDays: { type: [Number], required: true},
+  user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+   } 
+})
 
 // 3. Create a Model.
 export const Habit = model<IHabit>('Habit', habitSchema);
-
-/*
-run().catch(err => console.log(err));
-
-async function run() {
-  // 4. Connect to MongoDB
-  await connect('mongodb://127.0.0.1:27017/test');
-
-  const user = new User({
-    name: 'Bill',
-    email: 'bill@initech.com',
-    avatar: 'https://i.imgur.com/dM7Thhn.png'
-  });
-  await user.save();
-
-  console.log(user.email); // 'bill@initech.com'
-}
-*/
---
+++ server/src/routes/habits.ts
@@ -0,0 +1,13 @@
+import Router from 'express';
+import { Habit } from '../models/Habit';
+
+export const habitRoute = Router();
+
+habitRoute.post('/createhabit', async (req, res) => {
+    try{
+
+    }
+    catch(e) {
+        console.log(e);
+    }
+});
